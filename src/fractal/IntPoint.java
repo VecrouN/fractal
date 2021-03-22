771595e6a600fdace08a2f526acdb3ca015c3d4e
Vecrou_n <vnechaev00@gmail.com>
2021-03-22 15:52:38 +0300

    up

--- src/fractal/IntPoint.java
+++ src/fractal/IntPoint.java
@@ -1,17 +1,26 @@
 package fractal;
 
+import java.awt.Color;
+
 public class IntPoint {
     public final int x;
     public final int y;
-
+    Color color;
     public IntPoint() {
         x = 0;
         y = 0;
+        color = new Color(0,0,0);
     }
 
     public IntPoint(int x, int y) {
         this.x = x;
         this.y = y;
+        color = new Color(0,0,0);
+    }
+    public IntPoint(int x, int y, Color color) {
+        this.x = x;
+        this.y = y;
+        this.color = color;
     }
 
     @Override
--- src/fractal/main.java
+++ src/fractal/main.java
@@ -3,16 +3,57 @@ package fractal;
 import java.awt.image.BufferedImage;
 import javax.imageio.ImageIO;
 import java.io.File;
+import java.awt.Color;
+import java.io.IOException;
 
 public class main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 
 		int height = 1080;
 		int width = 1920;
+		BufferedImage test = ImageIO.	
+
+		try {
+			File file = new File("tmp/img1.jpg");
+			BufferedImage source = ImageIO.read(file);
+
+			BufferedImage result1 = new BufferedImage(source.getWidth(), source.getHeight(), source.getType());
+			BufferedImage result2 = new BufferedImage(source.getWidth(), source.getHeight(), source.getType());
+			Color color = new Color(0, 0, 0);
+			Color newColor = new Color(0, 0, 0);
+			int grey1 = 0;
+			int grey2 = 0;
+
+			for (int x = 0; x<result1.getWidth(); ++x) {
+				for (int y = 0; y < result1.getHeight(); ++y) {
+					color = new Color(source.getRGB(x, y));
+
+					grey1 = (int) (color.getRed() * 0.299 + color.getGreen() * 0.587 + color.getBlue() * 0.114);
+					grey2 = (int) ((color.getRed() + color.getGreen() + color.getBlue()) / 3);
+
+
+					newColor = new Color(grey1, grey1, grey1);
+					result1.setRGB(x, y, newColor.getRGB());
+
+					newColor = new Color(grey2, grey2, grey2);
+					result2.setRGB(x, y, newColor.getRGB());
+
+
+				}
+			}
+			File output = new File("tmp/img1_grey1.jpg");
+			ImageIO.write(result1, "jpg", output);
+
+			output = new File("tmp/img1_grey2.jpg");
+			ImageIO.write(result2, "jpg", output);
+
+		} catch (IOException e)
+		{
+			System.out.println("Файл не найден или не удалось сохранить "+e);
+		}
 
 		System.out.println("LOG:\nwidth = " + width + "\nheight = " + height + "\n\n");
-		File file
 	}
 
 }
